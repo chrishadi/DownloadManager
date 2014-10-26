aee6cffae03026320dd2d0752e296036c15c223e
Christian Hadirahardja <chris.hadirahardja@yahoo.co.id>
2014-10-26 17:17:06 +0700

    Update TestDownloadManager.java

--- TestDownloadManager.java
+++ TestDownloadManager.java
@@ -1,7 +1,7 @@
 class TestDownloadManager {
 	public static void main(String[] args) {
 		DownloadManager dm = new DownloadManager();
-		dm.add("https://github.com/chrishadi/download-manager/archive/master.zip");
+		dm.add("https://github.com/chrishadi/DownloadManager/archive/master.zip");
 		dm.startAll();
 	}
 }
